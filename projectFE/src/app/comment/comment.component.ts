c23f705cf5a838e5e2a00fa4d3454bf4e4bdbd6c
NguyenVy-1512 <levychuyenvan@gmail.com>
2020-07-18 16:47:31 +0700

    sua commet

--- projectFE/src/app/comment/comment.component.ts
+++ projectFE/src/app/comment/comment.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-comment',
+  selector: 'comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.css'],
+  styles: [`
+  .star {
+  font-size: 30px;
+  color: #ebebeb;
+  }
+  .filled {
+  color: #FFD656;
+  }
+  `]
 })
 export class CommentComponent implements OnInit {
 
